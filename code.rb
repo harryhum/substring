042ab2b6e0a7e59bea89959c99e144fc8219fd4e
Harry Hum <harryhum22@hotmail.com>
2017-11-08 10:49:00 -0500

    Added multi word and downcase feature

--- code.rb
+++ code.rb
@@ -1,13 +1,17 @@
 def substrings(string, dictionary)
+    #if string has multiple words, break string down to words
+    string = string.split(' ')
     #breaks string down into an array of all possible substrings
     subs = []
-    letters = string.split('')
-    letters.each_index do |a|
-        letters.each_index do |b|
-            next unless b >= a
-            subs << letters[a..b].join('')
-        end
-    end 
+    string.each do |word|
+        letters = word.downcase.split('')
+        letters.each_index do |a|
+            letters.each_index do |b|
+                next unless b >= a
+                subs << letters[a..b].join('')
+            end
+        end 
+    end
     #iterate through each word in the substring to see if and how many times it matches
     matches = {}
     subs.each do |word|
@@ -22,3 +26,4 @@ end
 
 word_bank = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings('below', word_bank)
+substrings("Howdy partner, sit down! How's it going?", word_bank)
